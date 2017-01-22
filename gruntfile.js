c54355f074fc4661999327af8a8257efa93a70b8
datomo <dave.lengw@gmail.com>
2017-01-23 00:57:12 +0100

    added my gruntfile and the rest of the missing files

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,30 @@
 module.exports = function(grunt) {
 
-grunt.registerTask('speak',function (){
-	console.log("I'm speaking");
+
+grunt.initConfig({
+  concat: {
+    js: {
+      src: ['js/Vendor/1.js', 'js/Vendor/2.js', 'js/main.js'],
+      dest: 'dist/built.js',
+    },
+    css: {
+      src: ['css/Vendor/1.css', 'css/Vendor/2.css', 'css/main.css'],
+      dest: 'dist/style.css',
+    },
+  },
+  watch: {
+  js: {
+    files: ['js/**/*.js','js/*.js'],
+    tasks: ['concat:js'],
+  },
+  css: {
+    files: ['css/**/*.css','css/*.css'],
+    tasks: ['concat:css'],
+  },
+},
 });
 
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.registerTask('default',['concat','watch']);
 };
